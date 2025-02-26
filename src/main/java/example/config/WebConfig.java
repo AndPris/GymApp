58edec2356e480dab5b128dd918b4222b829941a
Andrii <andpris527@gmail.com>
2025-02-26 19:06:19 +0200

    Add Open API dependencies

--- src/main/java/example/config/WebConfig.java
+++ src/main/java/example/config/WebConfig.java
@@ -1,15 +1,26 @@
 package example.config;
 
 import example.logs.interceptors.LoggingInterceptor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "example")
+@ComponentScan(basePackages = {"org.springdoc", "example"})
+@Import({org.springdoc.core.configuration.SpringDocConfiguration.class, org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration.class,
+        org.springdoc.webmvc.ui.SwaggerConfig.class,
+        org.springdoc.core.properties.SwaggerUiConfigProperties.class,
+        org.springdoc.core.properties.SwaggerUiOAuthProperties.class,
+        org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class})
 public class WebConfig implements WebMvcConfigurer {
     private final LoggingInterceptor loggingInterceptor;
 
@@ -20,5 +31,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor).addPathPatterns("/**");
+    }
+
+    @Bean
+    public GroupedOpenApi dafault() {
+        return GroupedOpenApi.builder()
+                .group("all")
+                .packagesToScan("example.controllers")
+                .pathsToMatch("/**").build();
+    }
+
+    @Bean
+    public OpenAPI ApiInfo() {
+        return new OpenAPI()
+                .info(new Info().title("API")
+                        .description("Test api")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
--- src/main/java/example/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/example/controllers/RestResponseEntityExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {ValidationException.class, TrainerNotFoundException.class, TraineeNotFoundException.class,
             TrainingTypeNotFoundException.class, IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     protected ResponseEntity<Object> handleInvalidRequests(RuntimeException ex, WebRequest webRequest) {
         String body = ex.getMessage();
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, webRequest);
