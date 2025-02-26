116ee5d457b60ea4bce72af8324495bb2d2e5021
Andrii <andpris527@gmail.com>
2025-02-26 20:15:13 +0200

    Add documentation for user and training type controllers

--- src/main/java/example/controllers/TrainingTypeRestController.java
+++ src/main/java/example/controllers/TrainingTypeRestController.java
@@ -1,10 +1,17 @@
 package example.controllers;
 
+import example.dtos.trainee.TraineeDTO;
 import example.dtos.trainingtype.TrainingTypeDTO;
 import example.entities.TrainingType;
 import example.mappers.TrainingTypeMapper;
 import example.security.annotations.Authenticated;
 import example.services.TrainingTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -25,6 +32,15 @@ public class TrainingTypeRestController {
         this.trainingTypeMapper = trainingTypeMapper;
     }
 
+
+    @Operation(summary = "Get all training types")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All training types are returned",
+                    content = {@Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = TrainingTypeDTO.class)))}),
+            @ApiResponse(responseCode = "401", description = "Authentication failed",
+                    content = @Content),
+    })
     @Authenticated
     @GetMapping
     public ResponseEntity<List<TrainingTypeDTO>> getTrainingTypes(@RequestHeader(value = "Authorization") String authHeader) {
--- src/main/java/example/controllers/UserRestController.java
+++ src/main/java/example/controllers/UserRestController.java
@@ -1,9 +1,15 @@
 package example.controllers;
 
 import example.dtos.ChangePasswordDTO;
+import example.dtos.trainer.TrainerDTO;
 import example.security.annotations.Authenticated;
 import example.security.annotations.Authorized;
 import example.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +23,20 @@ public class UserRestController {
         this.userService = userService;
     }
 
+
+    @Operation(summary = "Update user password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Password successfully updated",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Authentication failed",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Authorization failed",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No user with such username",
+                    content = @Content),
+            @ApiResponse(responseCode = "422", description = "Invalid request body",
+                    content = @Content)
+    })
     @Authenticated
     @Authorized
     @PutMapping("/{username}/password")
